662c732f40dc486a943e2da5afd3e52dec43a2ac
Piyush Tanti <magicianworld99@gmail.com>
2021-12-20 21:40:01 +0530

    Server Created

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const userRouter = require('./routes/Users')
 const app = express();
-const http = require('http').Server(app);
-
+const server = require('http').Server(app);
+const socket = require('socket.io')(server, {
+    cors: {
+        origin: '*'
+    }
+});
 const PORT = process.env.PORT || 8000;
 
-app.use('/', userRouter)
+app.use('/api/user', userRouter)
 
-http.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is Starting at Port: ${PORT}`)
 })
